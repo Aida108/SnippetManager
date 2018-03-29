18656fb69ae1899ed1e629fbc3e73b2713679e4f
Adnan2110 <ady_hurtich@hotmail.com>
2018-03-29 14:41:57 +0200

    final changes

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'snippetmanager.eu.auth0.com',
   clientId: 'B2bZ1z530ogAlbPqLRx3Z88pUfRJlRTs',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://fathomless-hollows-69797.herokuapp.com/app/callback'
 }
